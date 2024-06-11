97d51d04244381e6f5a96432e55e51560b44019b
Amaralcode19 <lucasamaralap@outlook.com>
2024-06-11 15:45:51 -0300

    :wrench: minor-changes

--- lib/theme/design_system.dart
+++ lib/theme/design_system.dart
@@ -5,10 +5,10 @@ enum SystemColors { vividGamboge, eerieBlack, darkLiver, lightGray }
 extension ExtensionColors on SystemColors {
   Color toColor() {
     Map<SystemColors, Color> colorMap = {
-      SystemColors.lightGray: const Color(0xFFD4D4D2),
-      SystemColors.eerieBlack: const Color(0xFF1C1C1C),
-      SystemColors.darkLiver: const Color(0xFF505050),
-      SystemColors.vividGamboge: const Color(0xFFFF9500),
+      SystemColors.lightGray: const Color.fromRGBO(212, 212, 210, 1),
+      SystemColors.eerieBlack: const Color.fromRGBO(28, 28, 28, 1),
+      SystemColors.darkLiver: const Color.fromRGBO(80, 80, 80, 1),
+      SystemColors.vividGamboge: const Color.fromRGBO(255, 149, 0, 1),
     };
 
     return colorMap[this]!;
